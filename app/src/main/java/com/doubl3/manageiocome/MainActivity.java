74fcbdceda56ce6170bfb073a89d1fc48f7fe431
vandatptit <vandatlpt@gmail.com>
2021-11-06 10:59:43 +0700

    Add fragment to activity_main

--- app/src/main/java/com/doubl3/manageiocome/MainActivity.java
+++ app/src/main/java/com/doubl3/manageiocome/MainActivity.java
@@ -1,8 +1,17 @@
 package com.doubl3.manageiocome;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.doubl3.manageiocome.ViewModel.HistoryFragment;
+import com.doubl3.manageiocome.ViewModel.HomePageFragment;
+import com.doubl3.manageiocome.ViewModel.SettingsFragment;
+import com.doubl3.manageiocome.ViewModel.StatisticsFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +19,39 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.fragment_container, new HomePageFragment()).commit();
+        init();
+    }
+    void init() {
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment selectedFrag = null;
+                switch (item.getItemId()) {
+                    case R.id.navigation_home:
+                        selectedFrag = new HomePageFragment();
+                        break;
+                    case R.id.navigation_statistics:
+                        selectedFrag = new StatisticsFragment();
+                        break;
+                    case R.id.navigation_history:
+                        selectedFrag = new HistoryFragment();
+                        break;
+                    case R.id.navigation_settings:
+                        selectedFrag = new SettingsFragment();
+                        break;
+                    default:
+                        selectedFrag = new HomePageFragment();
+                        break;
+                }
+                assert  selectedFrag != null;
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.fragment_container, selectedFrag).commit();
+                return true;
+            }
+        });
     }
 }
--- app/src/main/java/com/doubl3/manageiocome/ViewModel/HistoryFragment.java
+++ app/src/main/java/com/doubl3/manageiocome/ViewModel/HistoryFragment.java
@@ -1,4 +1,33 @@
 package com.doubl3.manageiocome.ViewModel;
 
-public class HistoryFragment {
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.doubl3.manageiocome.R;
+
+public class HistoryFragment extends Fragment {
+    private final String TAG = "HistoryFragment";
+
+    public HistoryFragment(){
+
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        Log.d(TAG, "onCreate: ");
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        Log.d(TAG, "onCreateView: ");
+        return inflater.inflate(R.layout.fragment_history, container, false);
+    }
 }
